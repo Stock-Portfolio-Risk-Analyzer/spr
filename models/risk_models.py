510445a9c89bb9e0c4199a25ef9e5c816d79685a
Thibaut Xiong <txiong3@illinois.edu>
2016-02-07 16:36:02 -0600

    add get_volatility()

--- models/risk_models.py
+++ models/risk_models.py
@@ -3,6 +3,22 @@ import pandas as pd
 from datautils.yahoo_finance import get_pct_returns
 from datetime import datetime as dt
 
+def get_alpha(symbol, start_date=dt(year=2013, month=1, day=1), end_date=dt.today(), index_symbol='^GSPC'):
+    """
+    Using Jensen's alpha (https://en.wikipedia.org/wiki/Jensen%27s_alpha)
+    :param symbol:
+    :param start_date:
+    :param end_date:
+    :param index_symbol:
+    :return:
+    """
+    symbol_returns = get_pct_returns(symbol, start_date, end_date)
+    index_returns = get_pct_returns(index_symbol, start_date, end_date)
+    beta = get_beta(symbol, start_date, end_date, index_symbol)
+    alpha = np.mean(symbol_returns)-beta*np.mean(index_returns)
+
+    return alpha*100
+
 def get_beta(symbol, start_date=dt(year=2013, month=1, day=1), end_date=dt.today(), index_symbol='^GSPC'):
     """
 
@@ -22,9 +38,10 @@ def get_beta(symbol, start_date=dt(year=2013, month=1, day=1), end_date=dt.today
 
     return beta
 
-def get_alpha(symbol, start_date=dt(year=2013, month=1, day=1), end_date=dt.today(), index_symbol='^GSPC'):
+
+def get_volatility(symbol, start_date=dt(year=2013, month=1, day=1), end_date=dt.today(), index_symbol='^GSPC'):
     """
-    Using Jensen's alpha (https://en.wikipedia.org/wiki/Jensen%27s_alpha)
+    Standard deviation of returns
     :param symbol:
     :param start_date:
     :param end_date:
@@ -33,11 +50,14 @@ def get_alpha(symbol, start_date=dt(year=2013, month=1, day=1), end_date=dt.toda
     """
     symbol_returns = get_pct_returns(symbol, start_date, end_date)
     index_returns = get_pct_returns(index_symbol, start_date, end_date)
-    beta = get_beta(symbol, start_date, end_date, index_symbol)
-    alpha = np.mean(symbol_returns)-beta*np.mean(index_returns)
+    cov_mat = np.cov(symbol_returns, index_returns)
 
-    return alpha*100
+    volatility = np.sqrt(cov_mat[0, 0])
+
+    # TODO: is this right?
+    return volatility*100
 
 if __name__ == "__main__":
     # print get_beta('AAPL')
-    print get_alpha('FB')
+    # print get_alpha('FB')
+    print get_volatility('FB')
