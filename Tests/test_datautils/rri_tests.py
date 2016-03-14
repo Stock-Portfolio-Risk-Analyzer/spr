f3b77a00dcbccb052e79327cafb68e3d8fd05d30
Shivam Gupta <shivamvmc@gmail.com>
2016-03-14 00:53:08 -0500

    Added 3 RRI test cases - Shivam
    Added test file for RRI - Shivam Gupta

---
+++ Tests/test_datautils/rri_tests.py
@@ -0,0 +1,28 @@
+import unittest
+from datetime import datetime as dt
+from datautils.rri import *
+
+class TestRRI(unittest.TestCase):
+
+    def test_compute_portfolio_rri_for_range1(self):
+    	stock_list = ["AAPL", "NFLX", "FB"]
+    	quantity_list = [10, 10, 10]
+    	start_date = "03/03/2016"
+    	end_date = "03/13/2016"
+    	rri = compute_portfolio_rri_for_range(stock_list, quantity_list, start_date, end_date)
+    	self.assertEqual(rri, 1.48118245233)
+
+    def test_compute_portfolio_rri_for_range2(self):
+    	stock_list = ["AAPL", "NFLX", "FB"]
+    	quantity_list = [0, 0, 0]
+    	start_date = "03/03/2016"
+    	end_date = "03/13/2016"
+    	rri = compute_portfolio_rri_for_range(stock_list, quantity_list, start_date, end_date)
+    	self.assertEqual(rri, nan) #TODO: <type 'numpy.float64'> mai nan kaise likhe?
+
+    def test_compute_stock_rri_for_range(self):
+    	symbol = "AAPL"
+    	start_date = "03/03/2016"
+    	end_date = "03/13/2016"
+    	rri = compute_stock_rri_for_range(symbol, start_date, end_date)
+    	self.assertEqual(rri, 1.73629103182)
