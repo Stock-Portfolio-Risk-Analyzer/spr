36223bec70990d13935eaddc6825b390f4d6f400
Shivam Gupta <shivamvmc@gmail.com>
2016-03-16 01:49:27 -0500

    Update rri_tests.py

--- Tests/test_datautils/rri_tests.py
+++ Tests/test_datautils/rri_tests.py
@@ -28,10 +28,19 @@ class TestRRI(unittest.TestCase):
     	rri = compute_portfolio_rri_for_range(stock_list, quantity_list, start_date, end_date)
     	self.assertEqual(rri, nan) #TODO: <type 'numpy.float64'> mai nan kaise likhe?
 
-    def test_compute_stock_rri_for_range(self):
+    def test_compute_stock_rri_for_range1(self):
         """ Tests the compute_stock_rri_for_range function """
     	symbol = "AAPL"
     	start_date = "03/03/2016"
     	end_date = "03/13/2016"
     	rri = compute_stock_rri_for_range(symbol, start_date, end_date)
     	self.assertEqual(rri, 1.73629103182)
+
+    def test_compute_stock_rri_for_range2(self):
+        """ Tests the compute_stock_rri_for_range function """
+    	symbol = "GDDY"
+    	start_date = "03/03/2016"
+    	end_date = "03/13/2016"
+    	rri = compute_stock_rri_for_range(symbol, start_date, end_date)
+    	self.assertEqual(rri, 1.73629103182)
+
